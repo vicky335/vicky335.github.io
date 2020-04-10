71a088db99e1619c86db6bb4649e7274780b43d4
vicky335 <vickyh335@gmail.com>
2020-04-11 01:26:55 +0800

    修改adsense

--- js/index.js
+++ js/index.js
@@ -41,14 +41,12 @@
           <h3 class="title">Advertisements</h3>
           <div class="content">
             <div class="adunit">
-              <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">\x3c/script>
               <ins class="adsbygoogle"
                 style="display:block; text-align:center;"
                 data-ad-layout="in-article"
                 data-ad-format="fluid"
                 data-ad-client="ca-pub-7900212994905360"
                 data-ad-slot="2530825933"></ins>
-              <script>(adsbygoogle = window.adsbygoogle || []).push({});\x3c/script>
             </div>
           </div>
         </div>
