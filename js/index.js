01810fee4e6c1fe795459d3e10d1747076842b4b
vicky335 <vickyh335@gmail.com>
2020-04-11 02:22:22 +0800

    修改adsense

--- js/index.js
+++ js/index.js
@@ -42,7 +42,7 @@
           <div class="content">
             <div class="adunit">
               <ins class="adsbygoogle"
-                style="display:block; text-align:center;"
+                style="display:block; text-align:center; margin: 0 auto;"
                 data-ad-layout="in-article"
                 data-ad-format="fluid"
                 data-ad-client="ca-pub-7900212994905360"
