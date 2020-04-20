0fff7daa5e7d951e8df81bac195b6bf4840016d2
VickyWong <vickyh335@gmail.com>
2020-04-20 18:12:23 +0800

    新增posts

--- js/index.js
+++ js/index.js
@@ -3,36 +3,23 @@
  */
 
 /* globals jQuery, document */
-(function($, undefined) {
+(function ($, undefined) {
   "use strict";
 
   var $document = $(document);
 
-  $document.ready(function() {
+  $document.ready(function () {
     var $postContent = $(".post-content");
     $postContent.fitVids();
 
     $(".scroll-down").arctic_scroll();
 
-    $(".menu-button[href='#'], .nav-cover, .nav-close").on("click", function(
+    $(".menu-button[href='#'], .nav-cover, .nav-close").on("click", function (
       e
     ) {
       e.preventDefault();
       $("body").toggleClass("nav-opened nav-closed");
     });
-
-    // ad
-    // var $postContentAd = $('.ui_adblock', '.post-content');
-    // if ($postContentAd.length > 0) {
-    //     var aids = $('meta[name="GOOGLEAD_POST_CONTENT"]').attr('content').split(',');
-    //     for (var i = 0, postContentAdLen = $postContentAd.length; i < postContentAdLen; i++) {
-    //         if (aids[i]) {
-    //             var html = ['<h3 class="title">Advertisements</h3><div class="content"><div class="adunit"><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js">\x3c/script>', '<ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-format="fluid" data-ad-layout="in-article" data-ad-client="ca-pub-7900212994905360" data-ad-slot="2530825933"></ins>', '<script>(adsbygoogle = window.adsbygoogle || []).push({});\x3c/script></div>\x3c/div>'].join('');
-    //             $postContentAd.eq(i).append(html);
-    //         }
-    //     }
-    // }
-    // ad end
 
     // ad
     if (typeof page !== "undefined" && page === "list") {
@@ -52,8 +39,7 @@
           </div>
         </div>
       `;
-      $("article.post").each(function(index) {
-        // if ((index % 3) - 2 == 0) {
+      $("article.post").each(function (index) {
         if ((index % 2) - 1 == 0) {
           const $this = $(this);
           $this.after(listAdHtml);
@@ -80,10 +66,14 @@
       `;
       let heightSum = 0;
       let adCount = 0;
+      let maxNum = 7;
+      if (!paltformOs.desktop) {
+        maxNum = 10;
+      }
       $(".post-content")
         .children()
         .not(".ui_adblock")
-        .each(function() {
+        .each(function () {
           let $this = $(this);
           heightSum += $this.height();
           if (heightSum > 500) {
@@ -94,7 +84,7 @@
             adCount++;
           }
 
-          if (adCount >= 7) {
+          if (adCount >= maxNum) {
             return false;
           }
         });
@@ -104,14 +94,14 @@
 
   // Arctic Scroll by Paul Adam Davis
   // https://github.com/PaulAdamDavis/Arctic-Scroll
-  $.fn.arctic_scroll = function(options) {
+  $.fn.arctic_scroll = function (options) {
     var defaults = {
         elem: $(this),
-        speed: 500
+        speed: 500,
       },
       allOptions = $.extend(defaults, options);
 
-    allOptions.elem.click(function(event) {
+    allOptions.elem.click(function (event) {
       event.preventDefault();
       var $this = $(this),
         $htmlBody = $("html, body"),
@@ -125,7 +115,7 @@
         toMove = parseInt(offset);
         $htmlBody.stop(true, false).animate(
           {
-            scrollTop: $(this.hash).offset().top + toMove
+            scrollTop: $(this.hash).offset().top + toMove,
           },
           allOptions.speed
         );
@@ -133,18 +123,58 @@
         toMove = parseInt(position);
         $htmlBody.stop(true, false).animate(
           {
-            scrollTop: toMove
+            scrollTop: toMove,
           },
           allOptions.speed
         );
       } else {
         $htmlBody.stop(true, false).animate(
           {
-            scrollTop: $(this.hash).offset().top
+            scrollTop: $(this.hash).offset().top,
           },
           allOptions.speed
         );
       }
     });
   };
+
+  const userAgent = navigator.userAgent;
+  let paltformOs = {};
+  paltformOs.fb =
+    userAgent.match(/FBAN/) || userAgent.match(/FBAV/) ? true : false;
+  paltformOs.line = userAgent.match(/Line\/([\d.]+)/) ? true : false;
+  paltformOs.msie = userAgent.match(/MSIE\/([\d.]+)/) ? true : false;
+  paltformOs.webkit = userAgent.match(/WebKit\/([\d.]+)/) ? true : false;
+  paltformOs.opera =
+    userAgent.match(/Opera Mobi/) || userAgent.match(/Opera.([\d.]+)/)
+      ? true
+      : false;
+  paltformOs.android =
+    userAgent.match(/(Android)\s+([\d.]+)/) ||
+    userAgent.match(/Silk-Accelerated/) ||
+    userAgent.match(/Android/) ||
+    userAgent.match(/Adr/)
+      ? true
+      : false;
+  paltformOs.ipad = userAgent.match(/(iPad).*OS\s([\d_]+)/) ? true : false;
+  paltformOs.iphone =
+    !paltformOs.ipad && userAgent.match(/(iPhone\sOS)\s([\d_]+)/)
+      ? true
+      : false;
+  paltformOs.webos = userAgent.match(/(webOS|hpwOS)[\s\/]([\d.]+)/)
+    ? true
+    : false;
+  paltformOs.touchpad =
+    paltformOs.webos && userAgent.match(/TouchPad/) ? true : false;
+  paltformOs.ios = paltformOs.ipad || paltformOs.iphone;
+  paltformOs.blackberry =
+    userAgent.match(/BlackBerry/) || userAgent.match(/PlayBook/) ? true : false;
+  paltformOs.fennec = userAgent.match(/fennec/i) ? true : false;
+  paltformOs.desktop = !(
+    paltformOs.ios ||
+    paltformOs.android ||
+    paltformOs.blackberry ||
+    paltformOs.opera ||
+    paltformOs.fennec
+  );
 })(jQuery);
